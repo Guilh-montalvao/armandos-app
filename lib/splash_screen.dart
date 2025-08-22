fcc16712a4de9e93f5e2afb42693dc261f861fac
Guilherme Augusto montalvão <guilherme06075828125@gmail.com>
2025-08-22 10:03:31 -0300

    feat: cria tela de splash com funco preto

---
+++ lib/splash_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:armandos/main.dart'; // Assuming your main content is in main.dart
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => SplashScreenState();
+}
+
+class SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+      Duration(seconds: 3),
+      () => Navigator.of(context).pushReplacement(MaterialPageRoute(
+        builder: (BuildContext context) => MyHomePage(title: 'Armandos App'),
+      )),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(),
+    );
+  }
+}
